f3e67906b6eb0b1a3d4b1682372c3c2e225e6892
Bruce Becker <bruce.becker@uefa.ch>
2020-08-22 12:22:27 +0200

    feat: Tutorial 22 goroutines
    Signed-off-by: Bruce Becker <bruce.becker@uefa.ch>

--- 022-goroutines.go
+++ 022-goroutines.go
@@ -1,7 +1,11 @@
 package main
 
+// https://gobyexample.com/goroutines
+
+// NOTE: a goroutine is a lightweight thread of execution
 import (
 	"fmt"
+	"time"
 )
 
 func f(from string) {
@@ -11,4 +15,27 @@ func f(from string) {
 }
 
 func main() {
+	// NOTE: Suppose we have a functionl call f(s).
+	// NOTE: Here's how we'd call that in the usual way, running it synchronously:
+	f("direct")
+
+	// NOTE: To invoke this function in a goroutein, use go f(s).
+	// NOTE: This new goroutine will execute concurrently with the calling one
+	go f("goroutine")
+
+	// NOTE: You can also start a goroutine for an anonymous function call:
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	// NOTE: Our two function calls are runnning asynchronously in separate goroutines now.
+	// NOTE: Wait for them to finish.
+	time.Sleep(time.Second)
+
+	fmt.Println("done")
+
+	// NOTE: When we run this program , we see the output of the blockjing call first,
+	// NOTE: then the output of the two goroutines..
+	// NOTE: This interleaving reflects the goroutines being run concurrently by the
+	// NOTE: go runtime.
 }
